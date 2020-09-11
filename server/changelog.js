9b8ee29342820adbc495bff9150ff50c5535d088
Vladimir Mandic <cyan00@gmail.com>
2020-09-11 15:35:52 -0400

    updated changelog generation

--- server/changelog.js
+++ server/changelog.js
@@ -1,27 +1,38 @@
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 const simpleGit = require('simple-git/promise');
-const log = require('@vladmandic/pilogger');
+const logger = require('@vladmandic/pilogger');
+
+const git = simpleGit();
 
 async function update(f) {
-  const git = simpleGit();
-  let text = '# PiProxy Change Log\n';
-  const gitLog = await git.log();
-  const sorted = gitLog.all.sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime()));
-  for (const entry of sorted) {
-    if (entry.refs !== '') {
-      let ver = entry.refs.split(' ');
-      ver = ver[ver.length - 1];
-      const dt = new Date(entry.date);
-      const date = `${dt.getMonth()}/${dt.getDate()}/${dt.getFullYear()}`;
-      if (ver !== 'master') text += `\n### **${ver}** ${date} ${entry.author_email}\n`;
-    } else if (entry.message !== '') {
-      text += `\n- ${entry.message}\n`;
+  let text = '# PiGallery Change Log\n';
+  const all = await git.log();
+  const log = all.all.sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime()));
+
+  let previous = '';
+  for (const l of log) {
+    const msg = l.message.toLowerCase();
+    if ((l.refs !== '') || msg.match(/^[0-9].[0-9].[0-9]/)) {
+      const dt = moment(l.date).format('YYYY/MM/DD');
+      const ver = msg.match(/[0-9].[0-9].[0-9]/) ? msg : l.refs;
+      text += `\n### **${ver}** ${dt} ${l.author_email}\n`;
+    } else if ((msg.length > 2) && !msg.startsWith('update') && (previous !== msg)) {
+      // console.log('text', previous, l.message, 'qqq');
+      previous = msg;
+      text += `- ${msg}\n`;
     }
   }
+
+  // process.stdout.write(text);
   const name = path.join(__dirname, '../', f);
   fs.writeFileSync(name, text);
-  log.state('Change log updated:', name);
+  logger.state('Change log updated:', name);
 }
 
 exports.update = update;
+
+if (!module.parent) {
+  update('CHANGELOG.md');
+}
